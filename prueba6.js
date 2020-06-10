dffa4fb0ae3f9063bef452a55b92b2a5fd8b42f3
JulianPuglia <julianpuglia@live.com>
2020-06-10 10:52:44 -0300

    agrego saludoArray

--- prueba6.js
+++ prueba6.js
@@ -1,1 +1,7 @@
 let prueba6= ["hola", "soy", "prueba","6", "test", "asd", "lista","testeo","bash","local"]
+
+function saludoArray(array){
+    return prueba6
+}
+
+console.log(saludoArray(prueba6));
